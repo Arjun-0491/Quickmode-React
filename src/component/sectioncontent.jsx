1e1a32c40ede948ef1deb731e4359423fb4cce3c
Arjun <arjunm977873@email.com>
2026-03-02 21:14:42 +0530

    text

--- src/component/sectioncontent.jsx
+++ src/component/sectioncontent.jsx
@@ -3,8 +3,8 @@
 
 export default  function SctionContent(){
     return      <div className=" ml-6  md:ml-8 lg:ml-25">
-            <p className="text-[1.5rem] text-[#ffcc99] font-medium">Delicious Cafe</p>
-            <h1 className="text-6xl font-bold text-white mt-3">Sweet Treats,<br></br>
+            <p className="text-2rem md:text-[1.5rem] text-[#ffcc99] font-medium ">Delicious Cafe</p>
+            <h1 className="text-2rem md:text-6xl lg:text-6x font-bold text-white mt-3">Sweet Treats,<br></br>
             Perfect Eats</h1>
            </div>
 }
